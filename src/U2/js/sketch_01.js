1d6f878d0d3add3e8f3f49a52134d8eac256bdfc
yanndepps <yann.deperrot@protonmail.com>
2023-01-22 15:54:14 +0100

    done sketch_01

--- src/U2/js/sketch_01.js
+++ src/U2/js/sketch_01.js
@@ -1,6 +1,6 @@
 /*
  * Sketching with Math and Quasi Physics
- * Sketch_01 -> Taming Randomness
+ * Sketch_01 -> Uniform Random
  * ©kynd
  */
 
@@ -15,10 +15,62 @@ const settings = {
 	animate: true
 };
 
+let record, max;
+let resolution = 150;
+
 const sketch = () => {
-	return ({ width, height }) => {
-		backround(33);
+	initRecord();
+	for (let i = 0; i < resolution * 10; i++) {
+		nextRandom();
+	}
+	//---
+	return ({ frame, width, height }) => {
+		clear();
+		background(254, 253, 183);
+		//---
+		let v;
+		for (let i = 0; i < resolution; i++) {
+			v = nextRandom();
+			line(v * width, 0, v * width, height);
+		}
+
+		fill(0);
+		stroke(0);
+		for (let i = 0; i < resolution; i++) {
+			let w = width / resolution;
+			let h = height / max * record[i];
+			let x = w * i;
+			let y = height - h;
+			rect(x, y, w, h);
+		}
+
+		if (frame % 600 == 0) {
+			initRecord();
+		}
 	};
 };
 
+// ---
+function initRecord() {
+	max = 1;
+	if (!record) {
+		record = [];
+	} else {
+		record.length = 0;
+	}
+
+	for (let i = 0; i < resolution; i++) {
+		record.push(0);
+	}
+}
+
+function nextRandom() {
+	let v = random(1);
+	if (++record[floor(v * resolution)] > max) { max++; }
+	return v;
+}
+
+
+
+// ---
 canvasSketch(sketch, settings);
