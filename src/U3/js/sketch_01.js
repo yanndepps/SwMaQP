254c8cf578f7cfde85072adb1440427cf3336ea3
yanndepps <yann.deperrot@protonmail.com>
2023-01-22 22:22:49 +0100

    done -> position

--- src/U3/js/sketch_01.js
+++ src/U3/js/sketch_01.js
@@ -15,10 +15,57 @@ const settings = {
 	animate: true
 };
 
+let position;
+let font;
+window.preload = () => {
+	font = loadFont('../../../assets/font/bauhaus.otf');
+}
+
 const sketch = () => {
+	position = createVector(100, -100);
 	return ({ width, height }) => {
-		background(33);
+		clear();
+		background(117, 147, 208);
+		//---
+		const hw = width / 2, hh = height / 2;
+
+		push();
+		translate(hw, hh);
+		fill(0);
+		stroke(0);
+		strokeWeight(2);
+		drawArrow(0, 0, position.x, position.y);
+		drawLabel(position.x, position.y, `position (${position.x.toPrecision(4)}, ${position.y.toPrecision(4)})`, LEFT);
+
+		strokeWeight(1);
+		line(-hw, 0, hw, 0);
+		line(0, -hh, 0, hh);
+		drawLabel(0, -4, 'Origin', LEFT);
+		pop();
+
+		position = createVector(mouseX - hw, mouseY - hh);
 	};
 };
 
+// ---
+function drawArrow(x0, y0, x1, y1) {
+	line(x0, y0, x1, y1);
+	let v = createVector(x1 - x0, y1 - y0).normalize();
+	line(x1, y1, x1 - v.y * 4 - v.x * 4, y1 + v.x * 4 - v.y * 4);
+	line(x1, y1, x1 + v.y * 4 - v.x * 4, y1 - v.x * 4 - v.y * 4);
+}
+
+function drawLabel(x, y, label, align = CENTER) {
+	push();
+	strokeWeight(0);
+	textFont(font);
+	textSize(24);
+	textAlign(align);
+	if (align == LEFT) { x += 6; }
+	if (align == RIGHT) { x -= 6; }
+	text(label, x, y);
+	pop();
+}
+
+// ---
 canvasSketch(sketch, settings);
